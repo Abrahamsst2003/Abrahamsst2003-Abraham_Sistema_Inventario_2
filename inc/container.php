e84a870930676c4274a3211028b3831b1079cdd4
Abrahamsst2003 <2asojo2003@gmail.com>
2025-06-04 11:37:16 -0400

    Se agrega el Proyecto

---
+++ inc/container.php
@@ -0,0 +1,11 @@
+</head>
+
+<body class="bg-gradient bg-dark bg-opacity-25">
+  <nav role="navigation" class="navbar navbar-dark navbar-static-top bg-info bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
